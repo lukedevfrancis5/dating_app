e26ca9a7f017d6a30ea56b46b5ab52a1c4b18c51
luke francis <lukedevfrancis@gmail.com>
2023-07-25 19:20:05 -0400

    added model fields

--- buildABoo/app/models.py
+++ buildABoo/app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    gender_choices = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+    )
+    gender = models.CharField(max_length=1, choices= gender_choices)
+    age = models.IntegerField()
+    
