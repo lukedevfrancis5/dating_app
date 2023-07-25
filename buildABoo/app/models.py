8e4bd7c0fbb43c844475e9389921a0d5ced567d0
luke francis <lukedevfrancis@gmail.com>
2023-07-25 19:27:06 -0400

    edited model fields

--- buildABoo/app/models.py
+++ buildABoo/app/models.py
@@ -1,13 +1,18 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 class User(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=50, blank=False)
+    last_name = models.CharField(max_length=50, blank=False)
     gender_choices = (
         ('M', 'Male'),
         ('F', 'Female'),
+        ('O', 'Other')
     )
     gender = models.CharField(max_length=1, choices= gender_choices)
-    age = models.IntegerField()
+    age = models.IntegerField(
+        default=18,
+        validators=[MaxValueValidator(100), MinValueValidator(18)]
+    )
     
