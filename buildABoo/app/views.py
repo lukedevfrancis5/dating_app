a2cd51895ead017e17fd1e68e0c66b3b1735a028
luke francis <lukedevfrancis@gmail.com>
2023-07-30 15:07:43 -0400

    worked on serializer

--- buildABoo/app/views.py
+++ buildABoo/app/views.py
@@ -3,6 +3,12 @@ from django.http import HttpResponse
 from django.views import View
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import UserSerializer
+from rest_framework import status 
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
+
+
 # Create your views here.
 
 @api_view(['POST'])
@@ -11,6 +17,14 @@ def login(request):
 
 @api_view(['POST'])
 def signup(request):
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        user = User.objects.get(username=request.data['username'])
+        user.set_password(request.data['password'])
+        user.save()
+        token = Token.objects.create(user=user)
+        return Response({'token': token.key, "user":serializer.data})
     return Response({})
 
 @api_view(['GET'])
