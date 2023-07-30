c02612e12b5cb84386378ab88b766de30f04b026
luke francis <lukedevfrancis@gmail.com>
2023-07-30 14:06:43 -0400

    added api decorators

--- buildABoo/app/views.py
+++ buildABoo/app/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 # Create your views here.
 
+@api_view(['POST'])
+def login(request):
+    return Response({})
+
+@api_view(['POST'])
+def signup(request):
+    return Response({})
+
+@api_view(['GET'])
+def test_token(request):
+    return Response({})
 
 class Sec_Page(View):
     def get(self, request):
