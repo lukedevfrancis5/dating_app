d319908e278d478528cec762057907d40a195769
luke francis <lukedevfrancis@gmail.com>
2023-07-29 23:53:06 -0400

    moved class to top of code

--- buildABoo/app/views.py
+++ buildABoo/app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.views import View
 # Create your views here.
+
+
+class Sec_Page(View):
+    def get(self, request):
+        return HttpResponse('This is the second page')
+    
+    
+def home_page_view(request):
+    return HttpResponse('This is the first page')
+
