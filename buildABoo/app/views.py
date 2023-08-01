86faa8636718ecb30273b804b43ede22ff0f368c
luke francis <lukedevfrancis@gmail.com>
2023-07-31 23:30:00 -0400

    worked on apis

--- buildABoo/app/views.py
+++ buildABoo/app/views.py
@@ -7,13 +7,18 @@ from .serializers import UserSerializer
 from rest_framework import status 
 from rest_framework.authtoken.models import Token
 from django.contrib.auth.models import User
-
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 
 @api_view(['POST'])
 def login(request):
-    return Response({})
+    user = get_object_or_404(User, username=request.date['username'])
+    if not user.check_password(request.data['password']):
+        return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
+    token, created = Token.objects.get_or_create(user=user)
+    serializer = UserSerializer(instance=user)
+    return Response({'token': token.key, "user":serializer.data})
 
 @api_view(['POST'])
 def signup(request):
@@ -25,7 +30,7 @@ def signup(request):
         user.save()
         token = Token.objects.create(user=user)
         return Response({'token': token.key, "user":serializer.data})
-    return Response({})
+    return Response(serializer.errors, status= status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET'])
 def test_token(request):
