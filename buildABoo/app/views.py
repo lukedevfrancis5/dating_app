f30c68496b84ee95ed68e69110511b580368265b
luke francis <lukedevfrancis@gmail.com>
2023-08-04 23:15:46 -0400

    fixed typo

--- buildABoo/app/views.py
+++ buildABoo/app/views.py
@@ -13,7 +13,7 @@ from django.shortcuts import get_object_or_404
 
 @api_view(['POST'])
 def login(request):
-    user = get_object_or_404(User, username=request.date['username'])
+    user = get_object_or_404(User, username=request.data['username'])
     if not user.check_password(request.data['password']):
         return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
     token, created = Token.objects.get_or_create(user=user)
